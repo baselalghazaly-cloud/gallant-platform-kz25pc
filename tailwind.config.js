7bac818258e3d4516a2f952c3e1a206f5766bf22
baselalghazaly-cloud <baselalghazaly@gmail.com>
2026-04-07 00:40:43 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // This tells Tailwind exactly where to look for your class names
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
